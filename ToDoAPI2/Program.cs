8a75ca92877c182a7e83466b0afa72f6471583d4
Kurtis Caldwell <KCaldwell86@outlook.com>
2023-07-17 02:01:21 -0500

    publish push

--- ToDoAPI2/Program.cs
+++ ToDoAPI2/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("OriginPolicy", "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("OriginPolicy", "http://todo.kurtiscaldwell.com", "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
     });
 });
 
